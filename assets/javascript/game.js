739c05095eae36153deca40a71cc6699d8ba2dd2
ManuCas <manucastle@hotmail.com>
2019-03-07 20:25:32 -0600

    all game converted in an object, ccs adjustmens to keep container inthe center of the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
-var listWords = ["staurday night", "be my lover", "it's my life",
-    "mr vain", "tonight's the nigth", "barbie girl",
+var listWords = ["saturday night", "be my lover", "it's my life",
+    "mr. vain", "tonight's the nigth", "barbie girl",
     "macarena", "rhythm is a dancer", "show me love",
     "gonna make you swet", "i'm too sexy", "i like to move it",
-    "we like to party", "another night"];
+    "we like to party", "another night","get ready for this",
+    "no limits", "what is love", "run away", "Rhythm of the Night",
+    "i'm the scatman"];
 const totalTries = 10;
 var gameWord;
 var wLength;
@@ -10,7 +12,6 @@ var template = [];
 var tries = 10;
 var choosenLetters = [];
 var wins = 0;
-var userLetter = "";
 var losses = 0;
 var correctSound = new Audio("assets/sounds/found.mp3");
 var incorrectSound = new Audio("assets/sounds/incorrect.mp3");
@@ -18,19 +19,19 @@ var winSound = new Audio("assets/sounds/win.mp3");
 var looseSound = new Audio("assets/sounds/loose.mp3");
 var startSound = new Audio("assets/sounds/start.mp3");
 //helps know if the game is alive or not
-var gameAlive = false;
+var gameAlive = true;
 
 //game begins
 getWord();
 
 document.onkeydown = function (event) {
-        if (tries > 0 && !gameAlive && !/[^a-zA-Z]/.test(event.key)) {
+        if (tries > 0 && gameAlive && !/[^a-zA-Z]/.test(event.key)) {
         letterFound(event.key.toUpperCase());
         if ((template.join("") == gameWord) && (tries > 0)) {
             winSound.play();
             wins++;
             document.getElementById("youwin-txt").innerHTML = "Awesome!!!";
-            gameAlive = true;
+            gameAlive = false;
             setTimeout(getWord, 3000);
         }
         else if (tries == 0) {
@@ -38,7 +39,7 @@ document.onkeydown = function (event) {
             losses++;
             document.getElementById("youloose-txt").innerHTML = "Try Again";
             document.getElementById("template-txt").innerHTML = gameWord;
-            gameAlive = true;
+            gameAlive = false;
             setTimeout(getWord, 3000);
         }
         stats();
@@ -46,21 +47,11 @@ document.onkeydown = function (event) {
 }
 
 ///game Functions
-
-//Update stats
-function stats() {
-    document.getElementById("tries-txt").innerHTML = tries;
-    document.getElementById("letters-txt").innerHTML = choosenLetters.join(" ");
-    document.getElementById("wins-txt").innerHTML = wins;
-    document.getElementById("losses-txt").innerHTML = losses;
-}
 //reset game & gets the word to guess
 function getWord() {
     tries = totalTries;
     template = [];
-    templateEmpty = [];
     choosenLetters = [];
-    templateGuessed = [];
     startSound.play();
     document.getElementById("youwin-txt").innerHTML = "";
     document.getElementById("youloose-txt").innerHTML = "";
@@ -69,9 +60,9 @@ function getWord() {
     wLength = gameWord.length;
     createTemplate();
     stats();
-    gameAlive = false;
+    gameAlive = true;
 }
-//creats the tamplate whit the underscores
+//creates the tamplate whit the underscores
 function createTemplate() {
     for (i = 0, j = wLength; i < j; i++) {
         if (gameWord[i] === " ") {
@@ -81,24 +72,25 @@ function createTemplate() {
 
             template.push("'");
         }
+        else if (gameWord[i] === ".") {
+
+            template.push(".");
+        }
         else {
             template.push("_");
         }
     }
     document.getElementById("template-txt").innerHTML = template.join("");
 }
-//compares the letter from user with the word and psh it to the Guessed letters array
+//compares the letter from user with the word and pushes it to the Guessed letters array
 function letterFound(letter) {
     var t = 0;
-    console.log(choosenLetters.join(""));
-    console.log(choosenLetters.join(letter));
     if (tries > 0) {
         if (choosenLetters.includes(letter)) {
             tries++;
         }
         else {
             for (i = 0, j = wLength; i < j; i++) {
-
                 if (gameWord[i] == letter) {
                     correctSound.play();
                     template[i] = letter;
@@ -115,7 +107,13 @@ function letterFound(letter) {
     }
 }
 
-
+//Update stats
+function stats() {
+    document.getElementById("tries-txt").innerHTML = tries;
+    document.getElementById("letters-txt").innerHTML = choosenLetters.join(" ");
+    document.getElementById("wins-txt").innerHTML = wins;
+    document.getElementById("losses-txt").innerHTML = losses;
+}
 
 
 
---
+++ assets/javascript/objectgame.js
@@ -0,0 +1,136 @@
+//Game object
+var game = {
+    template: [],
+    choosenLetters: [],
+    gameWord: "",
+    wLength: 0,
+    tries: 10,
+    wins: 0,
+    losses: 0,
+    gameAlive: true,
+    totalTries: 10,
+    correctSound: new Audio("assets/sounds/found.mp3"),
+    incorrectSound: new Audio("assets/sounds/incorrect.mp3"),
+    winSound: new Audio("assets/sounds/win.mp3"),
+    looseSound: new Audio("assets/sounds/loose.mp3"),
+    startSound: new Audio("assets/sounds/start.mp3"),
+    listWords: ["saturday night", "be my lover", "it's my life",
+        "mr. vain", "tonight's the nigth", "barbie girl",
+        "macarena", "rhythm is a dancer", "show me love",
+        "gonna make you swet", "i'm too sexy", "i like to move it",
+        "we like to party", "another night", "get ready for this",
+        "no limits", "what is love", "run away", "Rhythm of the Night",
+        "i'm the scatman"],
+    //reset game & gets the word to guess
+    getWord: function () {
+        game.tries = game.totalTries;
+        game.template = [];
+        game.choosenLetters = [];
+        game.startSound.play();
+        document.getElementById("youwin-txt").innerHTML = "";
+        document.getElementById("youloose-txt").innerHTML = "";
+        game.gameWord = game.listWords[Math.floor(Math.random() * game.listWords.length)].toUpperCase();
+        console.log(game.gameWord);
+        game.wLength = game.gameWord.length;
+        game.createTemplate();
+        game.stats();
+        game.gameAlive = true;
+    },
+    //creates the tamplate whit the underscores
+    createTemplate: function () {
+        for (i = 0, j = game.wLength; i < j; i++) {
+            if (game.gameWord[i] === " ") {
+                game.template.push(" ");
+            }
+            else if (game.gameWord[i] === "'") {
+
+                game.template.push("'");
+            }
+            else if (game.gameWord[i] === ".") {
+
+                game.template.push(".");
+            }
+            else {
+                game.template.push("_");
+            }
+        }
+        document.getElementById("template-txt").innerHTML = game.template.join("");
+    },
+    //compares the letter from user with the word and pushes it to the Guessed letters array
+    letterFound: function (letter) {
+        var t = 0;
+        if (game.tries > 0) {
+            if (game.choosenLetters.includes(letter)) {
+                game.tries++;
+            }
+            else {
+                for (i = 0, j = game.wLength; i < j; i++) {
+                    if (game.gameWord[i] == letter) {
+                        game.correctSound.play();
+                        game.template[i] = letter;
+                        t++;
+                    }
+                }
+                game.choosenLetters.push(letter);
+                document.getElementById("template-txt").innerHTML = game.template.join("");
+            }
+            if (t == 0) {
+                game.tries--;
+                game.incorrectSound.play();
+            }
+        }
+    },
+    //update game stats
+    stats: function () {
+        document.getElementById("tries-txt").innerHTML = game.tries;
+        document.getElementById("letters-txt").innerHTML = game.choosenLetters.join(" ");
+        document.getElementById("wins-txt").innerHTML = game.wins;
+        document.getElementById("losses-txt").innerHTML = game.losses;
+    },
+    //main logic of the game
+    mainLogic: function () {
+        if (game.tries > 0 && game.gameAlive && !/[^a-zA-Z]/.test(event.key)) {
+            game.letterFound(event.key.toUpperCase());
+            if ((game.template.join("") == game.gameWord) && (game.tries > 0)) {
+                game.winSound.play();
+                game.wins++;
+                document.getElementById("youwin-txt").innerHTML = "Awesome!!!";
+                game.gameAlive = false;
+                setTimeout(game.getWord, 3000);
+            }
+            else if (game.tries == 0) {
+                game.looseSound.play();
+                game.losses++;
+                document.getElementById("youloose-txt").innerHTML = "Try Again";
+                document.getElementById("template-txt").innerHTML = game.gameWord;
+                game.gameAlive = false;
+                setTimeout(game.getWord, 3000);
+            }
+            game.stats();
+        }
+    },
+}
+
+//game begins
+game.getWord();
+
+document.onkeydown = function (event) {
+
+    game.mainLogic();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
